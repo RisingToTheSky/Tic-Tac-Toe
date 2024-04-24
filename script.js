8767667903efdc10beff500b60525ace2b06a047
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-04-24 14:18:56 +0200

    Create player and game object

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 (function gameBoard () {
     const board = [];
-    for (let i = 0; i < 3; i++) {
+    const rows = 3;
+    const columns = 3;
+
+    for (let i = 0; i < columns; i++) {
         board[i] = [];
-        for (let j = 0; j < 3; j++) {
-            board[i][j] = "";
+        for (let j = 0; j < rows; j++) {
+            board[i].push(Cell());
         }
     }
     console.log(board);
+    return board;
 })();
+
+function Player() {
+    const players = [{
+        name: "Joshua", // prompt("Player 1!  Name:"),
+        icon: "X",
+    }, {
+        name: "Esther", // prompt("Player 2!  Name:"),
+        icon: "O",
+    }]
+    return players;
+}
+console.log(Player());
+
+function Game() {
+
+}
