24d078b59ebfa82d689916d28ece67aa58494b2b
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-04-24 13:31:22 +0200

    Make a gameboard in the console

---
+++ script.js
@@ -0,0 +1,10 @@
+(function gameBoard () {
+    const board = [];
+    for (let i = 0; i < 3; i++) {
+        board[i] = [];
+        for (let j = 0; j < 3; j++) {
+            board[i][j] = "";
+        }
+    }
+    console.log(board);
+})();
