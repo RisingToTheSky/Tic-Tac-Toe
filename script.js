d955ee386fbd7b10db57ae44c55e7c36a2a5bbd1
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-06-03 11:32:50 +0200

    Add a resetBoard function

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function Player1() {
-    const name = "Joshua";
+    const name = "Player 1";
     const icon = "X";
     return {name, icon};
 }
 
 function Player2() {
-    const name = "Esther";
+    const name = "Player 2";
     const icon = "O";
     return {name, icon};
 }
@@ -26,78 +26,90 @@ const gameBoard = (function() {
 })();
 console.log(gameBoard);
 
-function placeIcon(gameBoard, playerIcon) {
-    let choice;
-    let validChoice = false;
-
-    while (!validChoice) {
-        choice = prompt("Give me a number between 1 and 9");
-        if (choice >= 1 && choice <= 9) {
-            let i = Math.floor((choice - 1) / 3);
-            let j = (choice - 1) % 3;
-            if (gameBoard[i][j] === "") {
-                gameBoard[i][j] = playerIcon;
-                validChoice = true;
-            } else {
-                choice = prompt("Give me a number between 1 and 9")
-            }
-        }
-    }
-}
-
-function changeTurn() {
-    let turn = 0;
-    for (let i = 0; i < 9; i++) {
-        if (turn === 0) {
-            placeIcon(gameBoard, Player1().icon);
-            if (checkWinner(gameBoard, Player1().icon) === true) {
-                console.log(`${Player1().name} wins!`);
-                break;
-            };
-            turn++;
-        } else if (turn === 1) {
-            placeIcon(gameBoard, Player2().icon);
-            if (checkWinner(gameBoard, Player2().icon) === true) {
-                console.log(`${Player2().name} wins!`);
-                break;
-            };
-            turn--;
-        }
-    }
-}
-
-function checkWinner(gameBoard, playerIcon) {
-    const winningCombinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    const flatBoard = gameBoard.flat();
-
-    for (let combination of winningCombinations) {
-        let win = true;
-        for (let index of combination) {
-            if (flatBoard[index] !== playerIcon) {
-                win = false;
-                break;
-            }
-        }
-        if (win) {
-            return true;
-        }
-    }
-    return false;
-}
-
 // A display controller is for DOM
 
-
 // Game for winning combinations, changing turns, winner check
-function Game() {
+const Game = (function () {
+    function placeIcon(gameBoard, playerIcon) {
+        let choice;
+        let validChoice = false;
+    
+        while (!validChoice) {
+            choice = prompt("Give me a number between 1 and 9");
+            if (choice >= 1 && choice <= 9) {
+                let i = Math.floor((choice - 1) / 3);
+                let j = (choice - 1) % 3;
+                if (gameBoard[i][j] === "") {
+                    gameBoard[i][j] = playerIcon;
+                    validChoice = true;
+                } else {
+                    choice = prompt("Give me a number between 1 and 9")
+                }
+            }
+        }
+    }
+    
+    function changeTurn() {
+        let turn = 0;
+        for (let i = 0; i < 9; i++) {
+            if (turn === 0) {
+                placeIcon(gameBoard, Player1().icon);
+                if (checkWinner(gameBoard, Player1().icon) === true) {
+                    console.log(`${Player1().name} wins!`);
+                    resetBoard(gameBoard);
+                    break;
+                };
+                turn++;
+            } else if (turn === 1) {
+                placeIcon(gameBoard, Player2().icon);
+                if (checkWinner(gameBoard, Player2().icon) === true) {
+                    console.log(`${Player2().name} wins!`);
+                    resetBoard(gameBoard);
+                    break;
+                };
+                turn--;
+            } else {
+                console.log("Players tied!");
+                resetBoard(gameBoard);
+            }
+        }
+    }
+    
+    function checkWinner(gameBoard, playerIcon) {
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        const flatBoard = gameBoard.flat();
+        // Check if index of gameBoard array matches win condition
+        for (let combination of winningCombinations) {
+            let win = true;
+            for (let index of combination) {
+                if (flatBoard[index] !== playerIcon) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-}
+    function resetBoard(gameBoard) {
+        for (let i = 0; i < 3; i++) {
+            gameBoard[i] = [];
+            for (let j = 0; j < 3; j++) {
+                gameBoard[i].push("");
+            }
+        }
+    }
+    changeTurn();
+})();
